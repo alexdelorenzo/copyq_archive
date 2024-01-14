7cc332106b40b2d0ed28fea900d86c68c1c571d9
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2024-01-14 07:39:58 -0500

    Add description, minimum Python version

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 setup(
   name='copyq_archive',
   version='0.0.1',
-  description='CopyQ archive',
+  description='Archive CopyQ items to a SQLite database',
   author='Alex DeLorenzo <alex@alexdelorenzo.dev',
   packages=find_packages(),
   zip_safe=True,
+  python_requires='>=3.12',
 )
